2fdd0bd104d8717a9187731f8843608f913e8cf2
Brandon-Magofna <magofna68@gmail.com>
2021-06-02 10:35:16 -0700

    build out Models/Category.cs
    Co-Authored-By: Giancarlo Vigneri <bobloblaw.vigneri@gmail.com>

--- RecipeBox/Models/Category.cs
+++ RecipeBox/Models/Category.cs
@@ -1,7 +1,17 @@
-namespace ProjectName
-{
-  public class ClassName
-  {
+using System.Collections.Generic;
 
+namespace RecipeBox.Models
+{
+  public class Category
+  {
+    public Category()
+    {
+      this.JoinEntities = new HashSet<CategoryRecipe>();
+    }
+
+    public int CategoryId { get; set; }
+    public string Tag { get; set; }
+    public string Description { get; set; }
+    public virtual ICollection<CategoryRecipe> JoinEntities { get; set; }
   }
 }
--- RecipeBox/Models/DesignTimeDbContextFactory.cs
+++ RecipeBox/Models/DesignTimeDbContextFactory.cs
@@ -3,22 +3,22 @@ using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.Extensions.Configuration;
 using System.IO;
 
-namespace RecipieBox.Models
+namespace RecipeBox.Models
 {
-  public class RecipieBoxContextFactory : IDesignTimeDbContextFactory<RecipieBoxContext>
+  public class RecipeBoxContextFactory : IDesignTimeDbContextFactory<RecipeBoxContext>
   {
-    RecipieBoxContext IDesignTimeDbContextFactory<RecipieBoxContext>.CreateDbContext(string[] args)
+    RecipeBoxContext IDesignTimeDbContextFactory<RecipeBoxContext>.CreateDbContext(string[] args)
     {
       IConfigurationRoot configuration = new ConfigurationBuilder()
         .SetBasePath(Directory.GetCurrentDirectory())
         .AddJsonFile("appsettings.json")
         .Build();
 
-      var builder = new DbContextOptionsBuilder<RecipieBoxContext>();
+      var builder = new DbContextOptionsBuilder<RecipeBoxContext>();
 
       builder.UseMySql(configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(configuration["ConnectionStrings:DefaultConnection"]));
 
-      return new RecipieBoxContext(builder.Options);
+      return new RecipeBoxContext(builder.Options);
     }
   }
 }
